8a3de43df500e9ff66f1492897599971135bfdb2
Jason Kwok <JasonHK@users.noreply.github.com>
2020-08-02 17:30:54 +0800

    docs: added JavaDoc

--- src/main/java/net/jasonhk/minecraft/mods/torvaldsinput/exceptions/WhyYouInvokeThisShadowMethodException.java
+++ src/main/java/net/jasonhk/minecraft/mods/torvaldsinput/exceptions/WhyYouInvokeThisShadowMethodException.java
@@ -1,7 +1,14 @@
 package net.jasonhk.minecraft.mods.torvaldsinput.exceptions;
 
+/**
+ * Well, I don't know what on Earth to motivate you to invoke a method annotated with
+ * {@link org.spongepowered.asm.mixin.Shadow @Shadow}...love or responsibility?
+ */
 public class WhyYouInvokeThisShadowMethodException extends IllegalStateException
 {
+    /**
+     * Obtains an instance of {@code WhyYouInvokeThisShadowMethodException}.
+     */
     public WhyYouInvokeThisShadowMethodException()
     {
         super("You shouldn't invoke this @Shadow placeholder method, BUT YOU DID...for some reason.");
