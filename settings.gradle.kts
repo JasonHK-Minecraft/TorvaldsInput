49560df4e1296f4fe46f2c7f8f40514aa2ca6c86
Jason Kwok <JasonHK@users.noreply.github.com>
2020-08-04 16:39:41 +0800

    build: added commented configs for JNI, just in case we need it

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,15 @@
+@file:Suppress("PropertyName")
+
 pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
         maven("https://jitpack.io")
         maven("https://maven.fabricmc.net")
+
+        maven {
+            url = uri("https://dl.bintray.com/nokeedev/distributions")
+        }
     }
 
     resolutionStrategy {
@@ -12,6 +18,12 @@ pluginManagement {
                 "com.replaymod.preprocess" -> {
                     useModule("com.github.replaymod:preprocessor:${requested.version}")
                 }
+                else -> {
+                    if (requested.id.id.startsWith("dev.nokee."))
+                    {
+                        useModule("${requested.id.id}:${requested.id.id}.gradle.plugin:0.4.0")
+                    }
+                }
             }
         }
     }
@@ -19,14 +31,30 @@ pluginManagement {
 
 // rootProject.name = "TorvaldsInput"
 
-val TorvaldsInputVersions = listOf(
+val NativesVariants = listOf(
+        "2.0-2.9")
+
+val TorvaldsInputVariants = listOf(
         "1.11.2",
         "1.12.2")
 
-TorvaldsInputVersions.forEach { version ->
-    include(":$version")
-    project(":$version").apply {
-        projectDir = file("versions/$version")
+//include(":natives")
+//project(":natives").apply {
+//    projectDir = file("natives")
+//    buildFileName = "build.include.gradle.kts"
+//}
+//
+//NativesVariants.forEach { variant ->
+//    include(":natives:$variant")
+//    project(":natives:$variant").apply {
+//        projectDir = file("natives/$variant")
+//    }
+//}
+
+TorvaldsInputVariants.forEach { variant ->
+    include(":$variant")
+    project(":$variant").apply {
+        projectDir = file("versions/$variant")
         buildFileName = "../build.common.gradle"
     }
 }
