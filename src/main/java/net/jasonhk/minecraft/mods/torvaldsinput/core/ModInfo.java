e44e190d0dd7dd821a77e7c5db5019f8c557e5e7
Jason Kwok <JasonHK@users.noreply.github.com>
2020-08-07 20:57:10 +0800

    refactor: added `@UtilityClass` annotation

--- src/main/java/net/jasonhk/minecraft/mods/torvaldsinput/core/ModInfo.java
+++ src/main/java/net/jasonhk/minecraft/mods/torvaldsinput/core/ModInfo.java
@@ -1,17 +1,20 @@
 package net.jasonhk.minecraft.mods.torvaldsinput.core;
 
+import lombok.experimental.UtilityClass;
+
 /**
  * A utility class contains the basic information of this mod.
  */
+@UtilityClass
 public class ModInfo
 {
     /**
      * The identifier of this mod.
      */
-    public static final String ID = "torvaldsinput";
+    public final String ID = "torvaldsinput";
 
     /**
      * The name of this mod.
      */
-    public static final String NAME = "TorvaldsInput";
+    public final String NAME = "TorvaldsInput";
 }
